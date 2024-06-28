cf9e5ca9baf3ed9660fa66a7b9e0c90f60c84c98
Nabiur siddique <nabiursiddique01@gmail.com>
2024-06-28 22:21:55 +0600

    handled add todo modal

--- Todo-Redux/src/components/todo/TodoContainer.tsx
+++ Todo-Redux/src/components/todo/TodoContainer.tsx
@@ -1,12 +1,13 @@
 import AddTodoModal from "./AddTodoModal";
 import TodoCard from "./TodoCard";
+import TodoFilter from "./TodoFilter";
 
 const TodoContainer = () => {
   return (
     <div>
       <div className="flex justify-between mb-5">
         <AddTodoModal />
-        <button>Filter</button>
+        <TodoFilter />
       </div>
       <div className="bg-primary-gradient w-full h-full rounded-xl p-[5px]">
         <div className="bg-white p-5 w-full h-full rounded-lg space-y-3">
