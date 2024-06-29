423b093e602ec8214c63be9911bdaff91305a989
Nabiur siddique <nabiursiddique01@gmail.com>
2024-06-29 21:50:55 +0600

    redux cache is handled

--- Todo-Redux/src/components/todo/AddTodoModal.tsx
+++ Todo-Redux/src/components/todo/AddTodoModal.tsx
@@ -12,27 +12,51 @@ import {
 } from "../ui/dialog";
 import { Input } from "../ui/input";
 import { Label } from "../ui/label";
-import { useAppDispatch } from "@/redux/hook";
-import { addTodo } from "@/redux/features/todoSlice";
+import { useAddTodoMutation } from "@/redux/api/api";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "../ui/select";
 
 const AddTodoModal = () => {
   const [task, setTask] = useState("");
   const [description, setDescription] = useState("");
+  const [priority, setPriority] = useState("");
 
-  const dispatch = useAppDispatch();
+  //! For local state management
+  // const dispatch = useAppDispatch();
+
+  //* For server
+  //? [actualFunctionForPost,{data, isLoading, isError, isSuccess}]
+
+  const [addTodo, { data, isLoading, isError, isSuccess }] =
+    useAddTodoMutation();
+
+  console.log(data, isLoading, isError, isSuccess);
 
   const onSubmit = (e: FormEvent) => {
     e.preventDefault();
 
-    const randomString = Math.random().toString(36).substring(2, 7);
+    // const randomString = Math.random().toString(36).substring(2, 7);
 
     const taskDetails = {
-      id: randomString,
       title: task,
-      description: description,
+      description,
+      isCompleted: false,
+      priority,
     };
 
-    dispatch(addTodo(taskDetails));
+    console.log("inside modal", taskDetails);
+
+    //! For local state management
+    // dispatch(addTodo(taskDetails));
+
+    //* For server
+    addTodo(taskDetails);
   };
 
   return (
@@ -71,6 +95,22 @@ const AddTodoModal = () => {
                 id="description"
                 className="col-span-3"
               />
+            </div>
+
+            <div className="grid grid-cols-4 items-center gap-4">
+              <Label className="text-right">Priority</Label>
+              <Select onValueChange={(value) => setPriority(value)}>
+                <SelectTrigger className="col-span-3">
+                  <SelectValue placeholder="Select Priority" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectGroup>
+                    <SelectItem value="high">High</SelectItem>
+                    <SelectItem value="medium">Medium</SelectItem>
+                    <SelectItem value="low">Low</SelectItem>
+                  </SelectGroup>
+                </SelectContent>
+              </Select>
             </div>
           </div>
 
